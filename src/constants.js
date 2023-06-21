bba008ecad85c4b8bfe5a5fe6d589a040df18889
Maks <unreal9981@gmail.com>
2023-06-21 16:06:11 +0300

    currency icons

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 //prod
-const telegaToken = "5835524201:AAELin9i9ZXASMtObwVA9RGiUD8IJ2joBFA";
+// const telegaToken = "5835524201:AAELin9i9ZXASMtObwVA9RGiUD8IJ2joBFA";
+// test
+const telegaToken = "6087484133:AAEDMGJZ0wXk2lSSPTRNwEKtGpheJMZRLfs";
 
 //Test
 const SERVER_NAME = 'http://localhost:3003';
